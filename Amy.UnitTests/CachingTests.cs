d0a83414148d780a27ffcebe35a91d51f5cf179b
Gramli <Gramli@seznam.cz>
2020-03-13 09:57:43 +0100

    Added new tests

--- Amy.UnitTests/CachingTests.cs
+++ Amy.UnitTests/CachingTests.cs
@@ -7,7 +7,7 @@ using System.Linq;
 namespace Amy.UnitTests
 {
     [TestClass]
-    public class SmartFixedCollectionTests
+    public class CachingTests
     {
         int randomDataSize = 500;
         int colSize = 50;
@@ -40,6 +40,39 @@ namespace Amy.UnitTests
             col.Remove(lastItem);
             //get first and add
             string first = col.First();
+            col.Add(first);
+            //length should not change
+            Assert.AreEqual(colSize - 1, col.Count());
+        }
+
+        [TestMethod]
+        public void SmartFixedCollectionPairAdding()
+        {
+            var col = new SmartFixedCollectionPair<string, string>(colSize);
+
+            //create random data
+            var randomData = new List<int>(randomDataSize);
+            var random = new Random();
+            for (int i = 0; i < randomDataSize; i++)
+            {
+                randomData.Add(random.Next(0, 75));
+            }
+
+            //fill col
+            foreach (var item in randomData)
+            {
+                col.Add(item.ToString(), string.Empty);
+            }
+
+            //check count
+            Assert.IsTrue(col.Count() == this.colSize);
+            //check duplicates
+            Assert.IsFalse(col.GroupBy(x => x).Where(g => g.Count() > 1).Any());
+            //remove item
+            var lastItem = col.Last();
+            col.Remove(lastItem);
+            //get first and add
+            var first = col.First();
             col.Add(first);
             //length should not change
             Assert.AreEqual(colSize - 1, col.Count());
---
+++ Amy.UnitTests/EBNFTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Amy.UnitTests
+{
+    [TestClass]
+    public class EBNFTests
+    {
+        string character = "char = \"a\"|\"b\";";
+        string digit = "digit = \"1\"|\"2\";";
+        string intType = "intType = \"int\";";
+        string boolType = "boolType = \"bool\";";
+        string type = "type = intType | boolType";
+        string name = "name = char, { digit | char };";
+        string boolValue = "boolValue = \"true\" | \"false\";";
+        string boolVar = "boolVar = boolType,name, \"=\", boolValue;";
+        string intValue = "intValue = {digit};";
+        string intVar = "intVar = intType, name, \"=\", intValue;";
+        string variable = "variable = intVar | boolVar;";
+        string function = "function = [private],type, name , \"{\",{variable, \";\"},\"}\";";
+
+
+        [TestMethod]
+        public void ParsingTest()
+        { 
+            
+        }
+    }
+}
